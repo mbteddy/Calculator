c00032142855c7755a7922cdada901307f4c3199
Teddy Pappas <mbteddy@verizon.net>
2015-10-13 16:14:08 -0700

    Initial Commit

---
+++ Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by Teddy Pappas on 10/13/15.
+//  Copyright © 2015 Teddy Pappas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
